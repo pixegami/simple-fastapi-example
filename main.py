a6b5b44cf1a81ad026a213063c6b4e26373f1479
Jack (Pixegami) <pixegami@gmail.com>
2023-09-09 10:55:48 +1000

    add fastapi example

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ def root():
     return {"Hello": "World"}
 
 
-@app.post("/items/")
+@app.post("/items")
 def create_item(item: Item):
     items.append(item)
-    return item
+    return items
+
+
+@app.get("/items", response_model=list[Item])
+def list_items(limit: int = 10):
+    return items[0:limit]
 
 
 @app.get("/items/{item_id}", response_model=Item)
-def get_item(item_id: int):
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
-        raise HTTPException(status_code=404)
-
-
-@app.get("/items/", response_model=list[Item])
-def list_items(limit: int = 10):
-    return items[0:limit]
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
